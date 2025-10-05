9d661029ddc8fb0305633c28dc927f126950c0ff
Omkaar1234 <omkarkk004@gmail.com>
2025-10-05 14:58:42 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", // for local testing
-      "https://yumexpress.vercel.app/", // your frontend URL
+      "https://yumexpress.vercel.app", // your frontend URL
     ],
     credentials: true,
   })
