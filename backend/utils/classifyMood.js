f7a5a9b300708238039e274d7a9820288834f005
Omkaar1234 <omkarkk004@gmail.com>
2025-10-23 13:21:53 +0530

    token

--- backend/utils/classifyMood.js
+++ backend/utils/classifyMood.js
@@ -20,7 +20,7 @@ export const predictMood = async (text) => {
       { inputs: text },
       {
         headers: {
-          "Authorization": "Bearer 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",
+          "Authorization": "Bearer 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",
           "Content-Type": "application/json",
         },
       }
